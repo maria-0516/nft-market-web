b984dfd9eb3f2c8bb990fd72f568464b872a75fc
irina0516 <irina880011@gamil.com>
2023-03-13 15:45:43 +0900

    initial commit

---
+++ src/components/components/M_ItemdetailRedex.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const M_ItemdetailRedex = (props) => {
+    const { type, percent, per } = props;
+
+    return (
+        <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="nft_attr">
+                <span>{type}</span>
+                <div>
+                    <div>{per}</div>
+                    <p>{percent}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default M_ItemdetailRedex;
