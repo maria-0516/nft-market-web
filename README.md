81837b4d3f72d58856ec93018cd1f3537d19ad0f
DeamChain CEO <103885931+leopawel@users.noreply.github.com>
2023-03-19 15:48:10 +0100

    improve onchain feature

--- README.md
+++ README.md
@@ -1,3 +1,138 @@
-# PICASSO Marketplace Frontend
+# CNS Frontend
 
-This is PICASSO marketplace frontend.
+```ts
+const getAllOrders = async (storefront: any, page: number, limit: number) => {
+	const orders = await storefront.getOrders(page, limit)
+	return orders.map(i => ({id: Number(i[0]), price: i[5]})).filter(i => i.id !=0 )
+}
+
+const getOrdersByAddress = async (storefront: any, address: string, page: number, limit: number) => {
+	const ordersByOwner = await storefront.getOrdersByAddress(address, page, limit)
+	return ordersByOwner.map(i => Number(i[0])).filter(i => i !=0 );
+}
+
+const getBidsByAddress = async (storefront: any, address: string, page: number, limit: number) => {
+	const bids = await storefront.getBidsByAddress(address, page, limit)
+	return bids.map(i => ({id: Number(i[0]), /* address: i[2],  */price: ethers.utils.formatUnits(i[4], 18)})).filter(i => i.id !=0 );
+}
+
+const test = async (storefront: any) => {
+	try {
+		const [owner] = await ethers.getSigners();
+		
+		// deploy NFT contracts
+		const usdt = await deployContract("TetherToken", [N(1e8), "Tether USD", "USDT", 6]);
+		const nft = await deployContract("NFT", ["test1 NFT", "t1NFT"]);
+
+
+		// mint nft via NFT contracts
+		const tokenIds = [
+			"305638785357667841704819260044569792226553176877311822798918224005267718320",
+			"1047244300711308005502691544352853532897644948485530147806652033478270764633",
+			"217186629421343442058574315717344430350387817465866244494234513886299410962",
+			"44029076422232490676113501201379737341056420713462570111460414651147741889",
+			"354332744511276740242662285504744784660696550027049856620094052210367967889",
+			"721504116075778540988914843235400482006739517293910402816287200197276505111",
+			"173794838510765389710398181428030909850604475559401811567047620195722187011",
+			"284164939948106997506135635840128783333225700218939186691846467039566093341",
+			"253885747395429636230890330830742261025128284554244930512575486936917276822",
+			"684168767579123580215232683117056202536048369850128634938333879485821863336",
+			"917414890432902783721787213875523745311415191652851227107046393306453733247",
+			"429286934060636239444256046255241512105662385954349596568652644383873724621",
+			"130637526329616131658144818424303905943237629934140558972979359208001291518",
+			"739560381641203212243126268464268963092240187523454903658234487877802199183",
+			"2457430117691765919161633848808611230996640439582757182395106923844202509",
+		];
+		await sendRawTx(nft, "mintMultipleWithTokenId", ["https://localhost", tokenIds]);
+		
+		
+
+		
+
+		await sendRawTx(storefront, "addAcceptedToken", [usdt.address]);
+
+		await sendRawTx(nft, "approve", [storefront.address, tokenIds[0]]);
+		await sendRawTx(storefront, "createOrder", [nft.address, tokenIds[0], ZERO, N(0.001), EXPIRES]);
+		for (let k = 1; k < tokenIds.length; k++) {
+			await sendRawTx(nft, "approve", [storefront.address, tokenIds[k]]);
+			await sendRawTx(storefront, "createOrder", [nft.address, tokenIds[k], ZERO, N(0.001), EXPIRES]);
+		}
+
+		const addresses = {
+			storefront: storefront.address,
+			nft: nft.address,
+			tokenIds: tokenIds
+		};
+		await saveFiles("addresses.json", JSON.stringify(addresses, undefined, 4));
+
+		if (conf.local) {
+			// const now = currentTime()
+			// const _date = now - now % 86400;
+			// for (let k = 0; k < 5 ; k++) {
+			// 	const _buckets = await storefront.buckets(_date, k)
+			// 	const _orders = await storefront.orderById(_buckets)
+			// 	console.log('_buckets', _buckets)
+			// 	console.log('_orders', _orders)
+			// }
+
+			const orderIds = await getAllOrders(storefront, 0, 20)
+			console.log('orders', orderIds)
+
+			await sendRawTx(storefront, "cancelOrder", [orderIds[orderIds.length - 1].id]);
+			await sendRawTx(storefront, "updateOrder", [orderIds[0].id, N(0.0015), EXPIRES]);
+			
+
+			console.log('orders', await getAllOrders(storefront, 0, 10))
+			console.log('orders', await getAllOrders(storefront, 1, 10))
+			console.log('orders by address', await getOrdersByAddress(storefront, owner.address, 0, 20))
+
+			let _signer = null as any
+			if (conf.local) {
+				_signer = new ethers.Wallet(localnodeKeys[0], owner.provider)
+			} else {
+				_signer = new ethers.Wallet(conf.domainControllerKey, owner.provider)
+			}
+			// for (let k = 0; k < localnodeKeys.length; k++) {
+			// 	_signers.push(new ethers.Wallet(localnodeKeys[k], owner.provider))
+			// }
+
+			for (let k = 1; k < orderIds.length - 1; k++) { // 
+				await sendRawTx(storefront.connect(_signer), "placeBid", [orderIds[k].id, N(0.001), EXPIRES], N(0.001));
+			}
+			console.log("balance ", _signer.address, ethers.utils.formatEther(await _signer.getBalance()))
+			await sendRawTx(storefront.connect(_signer), "placeBid", [orderIds[1].id, N(0.0009), EXPIRES]);
+			
+			const bids1_1 = await getBidsByAddress(storefront, _signer.address, 0, 2)
+			console.log('bids_1', bids1_1)
+			console.log("balance ", _signer.address, ethers.utils.formatEther(await _signer.getBalance()))
+			
+			await sendRawTx(storefront.connect(_signer), "placeBid", [orderIds[1].id, N(0.0011), EXPIRES], N(0.0002));
+			const bids1_2 = await getBidsByAddress(storefront, _signer.address, 0, 2)
+			console.log('bids_2', bids1_2)
+			console.log("balance ", _signer.address, ethers.utils.formatEther(await _signer.getBalance()))
+			await sendRawTx(storefront.connect(_signer), "cancelBid", [bids1_2[0].id]);
+
+			await sendRawTx(storefront.connect(_signer), "executeOrder", [orderIds[0].id, N(0.0015)], N(0.0015));
+			console.log('executeOrder', await getAllOrders(storefront, 0, 20))
+
+			await sendRawTx(storefront, "acceptBid", [bids1_2[1].id]);
+
+			const bidCount = await storefront.getBidCountByAddress(_signer.address);
+			console.log('bidCount', bidCount)
+			const bids1 = await getBidsByAddress(storefront, _signer.address, 0, 5)
+			console.log('bids - 1', bids1)
+			const bids2 = await getBidsByAddress(storefront, _signer.address, 1, 5)
+			console.log('bids - 2', bids2)
+			const bids3 = await getBidsByAddress(storefront, _signer.address, 2, 5)
+			console.log('bids - 3', bids3)
+
+			console.log("balance ", owner.address, ethers.utils.formatEther(await owner.getBalance()))
+			await sendRawTx(storefront, "withdraw", [ZERO, owner.address]);
+			console.log("balance ", owner.address, ethers.utils.formatEther(await owner.getBalance()))
+		}
+		
+	} catch (error) {
+		console.log('test', error)
+	}
+}
+```
---
+++ src/contracts/contracts/Storefront.sol
@@ -0,0 +1,568 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
+
+library TransferHelper {
+	function safeTransfer(address token, address to, uint value) internal {
+		(bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+		require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FAILED');
+	}
+
+	function safeTransferFrom(address token, address from, address to, uint value) internal {
+		(bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+		require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FROM_FAILED');
+	}
+
+	function safeTransferETH(address to, uint value) internal {
+		(bool success,) = to.call{value:value}(new bytes(0));
+		require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
+	}
+}
+
+
+contract Storefront is Ownable, Pausable {
+	struct Bid {
+		uint id;// Bid Id
+		uint orderId;
+		address bidder;// Bidder address
+		address token;// accepted token for trading item
+		uint price;// Price for the bid in wei
+		uint expires;// Time when this bid ends
+		uint timestamp; // created time
+	}
+	
+	struct Order {
+		uint id; // // Order ID
+		address seller; // Owner of the NFT
+		uint assetId;
+		address collection;// NFT registry address
+		address acceptedToken;// accepted token for trading item
+		uint price;// Price (in wei) for the published item
+		uint expires;// Time when this sale ends
+		uint bidCount; // 
+		uint timestamp; // created time
+	}
+
+	// ORDER EVENTS
+	event OrderCreated(
+		uint id,
+		address indexed seller,
+		address indexed collection,
+		uint indexed assetId,
+		address acceptedToken,
+		uint price,
+		uint expires
+	);
+
+	event OrderUpdated(uint id, uint price, uint expires);
+
+	event OrderSuccessful(
+		uint id,
+		address indexed buyer,
+		uint price
+	);
+
+	event OrderCancelled(uint id);
+
+	// BID EVENTS
+	event BidCreated(
+		uint id,
+		address indexed collection,
+		uint indexed assetId,
+		address indexed bidder,
+		uint price,
+		uint expires
+	);
+
+	event BidUpdated(uint id, uint price, uint expires);
+	event BidAccepted(uint id);
+	event BidCancelled(uint id);
+
+	event BuyCreated(
+		address indexed collection,
+		uint indexed assetId,
+		address indexed bidder,
+		address seller,
+		uint price
+	);
+
+	//using SafeERC20 for IERC20;
+
+	// address public weth;
+	mapping(address=>bool) public acceptedTokens;
+	uint public lastOrderId = 10000001;
+	uint public lastBidId = 20000001;
+	uint public constant MAX_BID_LIMIT = 100;
+
+	mapping(uint => Order) public orderById; // orderId => Order
+	mapping(uint => uint[]) public buckets; // timestamp => orderId[]
+	mapping(address => uint[]) ordersByOwner; // address => orderId[]
+	
+	mapping(uint => Bid) public bidById; // bidId => Bid
+	mapping(uint => uint[]) public bidsByOrderId; // orderId => bidId[]
+	mapping(address => uint[]) bidsByOwner; // address => orderId[]
+
+	//mapping(address => uint[]) getOrdersByOwner; // address => orderId[]
+
+	// From ERC721 registry assetId to Order (to avoid asset collision)
+	// mapping(address => mapping(uint => Order)) public orderByAssetId;
+
+	// From ERC721 registry assetId to Bid (to avoid asset collision)
+	// mapping(address => mapping(uint => Bid)) public bidByOrderId;
+
+	// 721 Interfaces
+	bytes4 public constant _INTERFACE_ID_ERC721 = 0x80ac58cd;
+
+	event ChangedFeePerMillion(uint256 cutPerMillion);
+
+    // Market fee on sales
+    uint256 public cutPerMillion=0;
+    uint256 public constant maxCutPerMillion = 100000; // 10% cut
+    
+    uint256 public royaltyPerMillion=0;
+	/**
+	 * @dev Initialize this contract. Acts as a constructor
+	 */
+	constructor() {
+		// weth = _weth;
+		// acceptedTokens.push(_weth);
+	}
+
+	
+
+    /**
+     * @dev Sets the share cut for the owner of the contract that's
+     *  charged to the seller on a successful sale
+     * @param _cutPerMillion - Share amount, from 0 to 99,999
+     */
+    function setOwnerCutPerMillion(uint256 _cutPerMillion) external onlyOwner {
+        require(
+            _cutPerMillion < maxCutPerMillion,
+            "The owner cut should be between 0 and maxCutPerMillion"
+        );
+
+        cutPerMillion = _cutPerMillion;
+        emit ChangedFeePerMillion(cutPerMillion);
+    }
+	// function setWETH(address _weth) public onlyOwner {
+	// 	weth = _weth;
+	// 	acceptedTokens.push(_weth);
+	// }
+
+	function addAcceptedToken(address _acceptedToken) public onlyOwner {
+		acceptedTokens[_acceptedToken] = true;
+	}
+
+	function removeAcceptedToken(address _acceptedToken) public onlyOwner {
+		delete acceptedTokens[_acceptedToken];
+	}
+
+	/**
+	 * @dev Sets the paused failsafe. Can only be called by owner
+	 * @param _setPaused - paused state
+	 */
+	function setPaused(bool _setPaused) public onlyOwner {
+		return (_setPaused) ? _pause() : _unpause();
+	}
+
+	function _deleteOrder(address _owner, uint _orderId, uint _timestamp) internal {
+		uint _bucket = _timestamp - _timestamp % 1 days;
+		uint _count = buckets[_bucket].length;
+		if (_count==0) {
+			delete buckets[_bucket];
+		} else if (buckets[_bucket][_count - 1]==_orderId) {
+			buckets[_bucket].pop();
+		} else {
+			for (uint k = 0; k < _count; k++) {
+				if (buckets[_bucket][k]==_orderId) {
+					buckets[_bucket][k] = buckets[_bucket][_count - 1];
+					buckets[_bucket].pop();
+					break;
+				}
+			}
+		}
+		_count = ordersByOwner[_owner].length;
+		if (_count==0) {
+			delete ordersByOwner[_owner];
+		} else if (ordersByOwner[_owner][_count - 1]==_orderId) {
+			ordersByOwner[_owner].pop();
+		} else {
+			for (uint k = 0; k < _count; k++) {
+				if (ordersByOwner[_owner][k]==_orderId) {
+					ordersByOwner[_owner][k] = ordersByOwner[_owner][_count - 1];
+					ordersByOwner[_owner].pop();
+					break;
+				}
+			}
+		}
+		delete orderById[_orderId];
+		// delete bidsByOrderId[_orderId];
+	}
+
+	function _deleteBid(address _bidder, uint _orderId, uint _bidId) internal {
+		uint _count = bidsByOwner[_bidder].length;
+		if (_count==1) {
+			delete bidsByOwner[_bidder];
+		} else if (bidsByOwner[_bidder][_count - 1] == _bidId) {
+			bidsByOwner[_bidder].pop();
+		} else {
+			for (uint k = 0; k < _count; k++) {
+				if (bidsByOwner[_bidder][k] == _bidId) {
+					bidsByOwner[_bidder][k] = bidsByOwner[_bidder][_count - 1];
+					bidsByOwner[_bidder].pop();
+					break;
+				}
+			}
+		}
+		// delete in bidsByOrderId
+		_count = bidsByOrderId[_orderId].length;
+		if (_count==1) {
+			delete bidsByOrderId[_orderId];
+		} else if (bidsByOrderId[_orderId][_count - 1] == _bidId) {
+			bidsByOrderId[_orderId].pop();
+		} else {
+			for (uint k = 0; k < _count; k++) {
+				if (bidsByOrderId[_orderId][k] == _bidId) {
+					bidsByOrderId[_orderId][k] = bidsByOrderId[_orderId][_count - 1];
+					bidsByOrderId[_orderId].pop();
+					break;
+				}
+			}
+		}
+		// delete bidById
+		delete bidById[_bidId];
+	}
+	function _deleteExpiredBucket() internal {
+		if (buckets[block.timestamp - 31 days].length!=0) delete buckets[block.timestamp - 31 days];
+	}
+	/**
+	 * @dev Internal function gets Order by nftRegistry and assetId. Checks for the order validity
+	 * @param _orderId - ID of order
+	 */
+	function _validateOrder(uint _orderId) internal view returns (Order memory order) {
+		order = orderById[_orderId];
+		require(order.id != 0, "Marketplace: asset not published");
+		require(order.expires >= block.timestamp, "Marketplace: order expired");
+	}
+
+	/**
+	 * @dev Creates a new order
+	 * @param _collection - Non fungible registry address
+	 * @param _assetId - ID of the published NFT
+	 * @param _acceptedToken - accepted token to buy NFT
+	 * @param _price - Price in Wei for the supported coin
+	 * @param _expires - Duration of the order (in hours)
+	 */
+	function createOrder(address _collection, uint _assetId, address _acceptedToken, uint _price, uint _expires) public whenNotPaused {
+		// Check nft registry
+		IERC721 nftRegistry = _requireERC721(_collection);
+		require(nftRegistry.ownerOf(_assetId) == msg.sender, "Marketplace: Only the asset owner can create orders");
+		require(_price > 0, "Marketplace: Price should be bigger than 0");
+		require(_expires > block.timestamp + 1 minutes, "Marketplace: expires should be more than 1 minute");
+		require(_expires <= block.timestamp + 180, "Marketplace: expires should be less than 180 days");
+		if (_acceptedToken!=address(0)) require(acceptedTokens[_acceptedToken], "Marketplace: accept token must be whitelisted");
+		// get NFT asset from seller
+		nftRegistry.transferFrom(msg.sender, address(this), _assetId);
+
+		// save order
+		orderById[lastOrderId] = Order({
+			id: lastOrderId,
+			seller: msg.sender,
+			collection: _collection,
+			assetId: _assetId,
+			acceptedToken: _acceptedToken,
+			price: _price,
+			bidCount: 0,
+			expires: _expires,
+			timestamp: block.timestamp
+		});
+		_deleteExpiredBucket();
+		buckets[block.timestamp - block.timestamp % 10 days].push(lastOrderId);
+		ordersByOwner[msg.sender].push(lastOrderId);
+		emit OrderCreated(lastOrderId, msg.sender, _collection, _assetId, _acceptedToken, _price, _expires);
+		lastOrderId++;
+	}
+
+	/**
+	 * @dev Cancel an already published order
+	 *  can only be canceled by seller or the contract owner
+	 * @param _orderId - ID of order
+	 */
+	function cancelOrder(uint _orderId) public whenNotPaused {
+		Order memory _order = _validateOrder(_orderId);
+        require(_order.seller == msg.sender || msg.sender == owner(), "Marketplace: unauthorized sender");
+
+        IERC721(_order.collection).transferFrom(address(this), msg.sender, _order.assetId);
+
+		_deleteOrder(_order.seller, _orderId, _order.timestamp);
+		emit OrderCancelled(_orderId);
+	}
+
+	/**
+	 * @dev Update an already published order, can only be updated by seller
+	 * @param _orderId - ID of the published NFT
+	 * @param _price - price
+	 * @param _expires - expires at
+	 */
+	function updateOrder(uint _orderId, uint _price, uint _expires) public whenNotPaused {
+		Order memory _order = _validateOrder(_orderId);
+		require(_order.id != 0, "Marketplace: asset not published");
+		require(_order.expires >= block.timestamp, "Marketplace: order expired");
+		// Order storage _order = _validateOrder(_orderId);
+        require(_order.seller == msg.sender || msg.sender == owner(), "Marketplace: unauthorized sender");
+		require(_price > 0, "Marketplace: Price should be bigger than 0");
+		require(_expires > block.timestamp + 1 minutes, "Marketplace: expires should be more than 1 minute");
+		require(_expires <= block.timestamp + 30 days, "Marketplace: expires should be less than 30 days");
+		// require(order.acceptedToken == _acceptedToken || order.bidCount==0, "Marketplace: order has bidders");
+		// order.acceptedToken = _acceptedToken;
+		orderById[_orderId].price = _price;
+		orderById[_orderId].expires = _expires;
+
+		emit OrderUpdated(_order.id, _price, _expires);
+	}
+
+	/**
+	 * @dev Executes the sale for a published NFT and checks for the asset fingerprint
+	 * @param _orderId - ID of order
+	 * @param _price - Order price
+	 */
+	function executeOrder(uint _orderId, uint _price) public payable whenNotPaused {
+		Order memory _order = _validateOrder(_orderId);
+		/// Check the execution price matches the order price
+		require(_order.price == _price, "Marketplace: invalid price");
+		uint _fee = _price * cutPerMillion / 1e6;
+		if (_order.acceptedToken == address(0)) {
+			require(_order.price == msg.value, "Marketplace: invalid price");
+			TransferHelper.safeTransferETH(_order.seller, _price - _fee);
+		} else {
+			TransferHelper.safeTransferFrom(_order.acceptedToken, msg.sender, address(this), _price);
+			TransferHelper.safeTransfer(_order.acceptedToken, _order.seller, _price - _fee);
+		}
+		IERC721(_order.collection).transferFrom(address(this), msg.sender, _order.assetId);
+		_deleteOrder(_order.seller, _orderId, _order.timestamp);
+		emit OrderSuccessful(_orderId, msg.sender, _price);
+	}
+
+	/**
+	 * @dev Places a bid for a published NFT and checks for the asset fingerprint
+	 * @param _orderId - ID of the published NFT
+	 * @param _price - Bid price in weth currency
+	 * @param _expires - Bid expiration time
+	 */
+	function placeBid(uint _orderId, uint _price, uint _expires) public payable whenNotPaused {
+		Order memory _order = _validateOrder(_orderId);
+		if (_expires > _order.expires) _expires = _order.expires;
+
+		uint _oldBidId;
+		for (uint k = 0; k < bidsByOwner[msg.sender].length; k++) {
+			if (bidById[bidsByOwner[msg.sender][k]].orderId == _orderId) {
+				_oldBidId = bidsByOwner[msg.sender][k];
+				break;
+			}
+		}
+		if (_oldBidId!=0) {
+			uint _oldPrice = bidById[_oldBidId].price;
+			if (_order.acceptedToken == address(0)) {
+				uint _value = _oldPrice + msg.value;
+				require(_value >= _price, "Marketplace: bid count reached limitation");
+				if (_value - _price > 1e14) { // 0.0001
+					// refund extra
+					TransferHelper.safeTransferETH(msg.sender, _value - _price);
+				}
+			} else {
+				if (_oldPrice > _price) {
+					TransferHelper.safeTransfer(_order.acceptedToken, msg.sender, _oldPrice - _price);
+				} else if (_oldPrice < _price) {
+					TransferHelper.safeTransferFrom(_order.acceptedToken, msg.sender, address(this), _price - _oldPrice);
+				}
+			}
+			bidById[_oldBidId].price = _price;
+			bidById[_oldBidId].expires = _expires;
+            emit BidUpdated(_oldBidId, _price, _expires);
+		} else {
+			require(_order.bidCount < MAX_BID_LIMIT, "Marketplace: bid count reached limitation");
+			require(bidsByOwner[msg.sender].length < MAX_BID_LIMIT, "Marketplace: bid count reached limitation");
+
+			if (_order.acceptedToken == address(0)) {
+				require(msg.value == _price, "Marketplace: invalid price");
+			} else {
+				TransferHelper.safeTransferFrom(_order.acceptedToken, msg.sender, address(this), _price);
+			}
+			_order.bidCount++;
+			bidById[lastBidId] = Bid({
+				id: lastBidId,
+				orderId: _orderId,
+				bidder: msg.sender,
+				token: _order.acceptedToken,
+				price: _price,
+				expires: _expires,
+				timestamp: block.timestamp
+			});
+			bidsByOrderId[_orderId].push(lastBidId);
+			bidsByOwner[msg.sender].push(lastBidId);
+
+            emit BidCreated(
+                lastBidId,
+                _order.collection,
+                _order.assetId,
+                msg.sender, // bidder
+                _price,
+                _expires
+            );
+            lastBidId++;
+		}
+
+		
+	}
+
+	/**
+	 * @dev Cancel an already published bid, can only be canceled by seller or the contract owner
+	 * @param _bidId - ID of the published NFT
+	 */
+	function cancelBid(uint _bidId) public whenNotPaused {
+		Bid memory _bid = bidById[_bidId];
+		require(_bid.id != 0, "Marketplace: invalid bid id");
+		// Order memory _order = orderById[_bid.orderId];
+		// require(_order.id != 0, "Marketplace: invalid bid id");
+		require(_bid.bidder == msg.sender || msg.sender == owner(), "Marketplace: Unauthorized sender");
+		// refund bid amount
+		if (_bid.token == address(0)) {
+			TransferHelper.safeTransferETH(_bid.bidder, _bid.price);
+		} else {
+			TransferHelper.safeTransfer(_bid.token, _bid.bidder, _bid.price);
+		}
+		_deleteBid(_bid.bidder, _bid.orderId, _bidId);
+		emit BidCancelled(_bidId);
+	}
+
+	/**
+	 * @dev Executes the sale for a published NFT by accepting a current bid
+	 * @param _bidId - ID of bid
+	 */
+	function acceptBid(uint _bidId) public whenNotPaused {
+		Bid memory _bid = bidById[_bidId];
+		require(_bid.id != 0, "Marketplace: bid invalid");
+		require(_bid.expires > block.timestamp, "Marketplace: bid expired");
+		Order memory _order = _validateOrder(_bid.orderId);
+		// item seller is the only allowed to accept a bid
+		require(_order.seller == msg.sender, "Marketplace: unauthorized sender");
+		uint _fee = _bid.price * cutPerMillion / 1e6;
+		if (_bid.token == address(0)) {
+			TransferHelper.safeTransferETH(_order.seller, _bid.price - _fee);
+		} else {
+			TransferHelper.safeTransfer(_bid.token, _order.seller, _bid.price - _fee);
+		}
+		IERC721(_order.collection).transferFrom(address(this), _bid.bidder, _order.assetId);
+		_deleteOrder(_order.seller, _bid.orderId, _order.timestamp);
+		_deleteBid(_bid.bidder, _bid.orderId, _bidId);
+		emit BidAccepted(_bidId);
+	}
+
+	/**
+	 * @dev fetch order list by page
+	 * @param _page - page number, started from zero
+	 * @param _limit - order count per a page
+	 */
+	function getOrders(uint _page, uint _limit) public view returns(Order[] memory _orders) {
+		uint _lastBucket = block.timestamp - block.timestamp % 10 days;
+		uint _startBucket = _lastBucket - 180 days; // max 30 days1
+		// uint _start = _page * _limit;
+		
+		uint _skip = _page * _limit;
+		uint _count = 0;
+
+		_orders = new Order[](_limit);
+		for (uint _bucket = _lastBucket; _bucket >= _startBucket; _bucket -= 10 days) {
+			uint _countInBucket = buckets[_bucket].length;
+			if (_countInBucket==0) continue ;
+            
+			for (uint k = 0; k < _countInBucket; k++) {
+				Order memory _order = orderById[buckets[_bucket][k]];
+				if (block.timestamp < _order.expires) {
+					if (_skip > 0) {
+						_skip--;
+					} else if (_count < _limit) {
+						_orders[_count++] = _order;
+					} else {
+						break;
+					}
+				}
+			}
+            if (_count==_limit) break;
+		}
+	}
+	
+	function getOrdersByAddress(address _owner, uint _page, uint _limit) public view returns(Order[] memory _orders) {
+		uint _start = _page * _limit;
+		uint _skip = 0;
+		uint _index = 0;
+		uint _count = ordersByOwner[_owner].length;
+		_orders = new Order[](_limit);
+		
+		for (uint k = 0; k < _count; k++) {
+			Order memory _order = orderById[ordersByOwner[_owner][k]];
+			if (block.timestamp < _order.expires) {
+				if (_skip < _start) {
+					_skip++;
+				} else if (_index < _limit) {
+					_orders[_index++] = _order;
+				} else {
+					break;
+				}
+			}
+		}
+	}
+	
+	function getBidCountByAddress(address _owner) public view returns(uint) {
+		return bidsByOwner[_owner].length;
+	}
+
+	// if bid expires is zero,  it means order is invalid.
+	function getBidsByAddress(address _owner, uint _page, uint _limit) public view returns(Bid[] memory _bids) {
+		// uint _start = _page * _limit;
+		uint _skip = _page * _limit;
+		uint _index = 0;
+		uint _count = bidsByOwner[_owner].length;
+		_bids = new Bid[](_limit);
+		
+		for (uint k = 0; k < _count; k++) {
+			Bid memory _bid = bidById[bidsByOwner[_owner][k]];
+			if (block.timestamp < _bid.expires) {
+				if (_skip > 0) {
+					_skip--;
+				} else if (_index < _limit) {
+					Order memory _order = orderById[_bid.orderId];
+					if (_order.id==0 || _order.expires < block.timestamp) {
+						_bid.expires = 0;
+					}
+					_bids[_index++] = _bid;
+				} else {
+					break;
+				}
+			}
+		}
+	}
+
+	function withdraw(address _token, address to) public onlyOwner {
+		require(to!=address(0), "Marketplace: should be non zero");		
+		if (_token==address(0)) {
+			(bool _result,) = to.call{value: address(this).balance}("");
+			require(_result, "refund ethers");
+		} else {
+			TransferHelper.safeTransfer(_token, to, IERC20(_token).balanceOf(address(this)));
+		}
+	}
+
+	function _requireERC721(address _collection) internal view returns (IERC721) {
+		// require(_collection.isContract(), "The NFT Address should be a contract");
+		require(IERC721(_collection).supportsInterface(_INTERFACE_ID_ERC721), "The NFT contract has an invalid ERC721 implementation");
+		return IERC721(_collection);
+	}
+}
